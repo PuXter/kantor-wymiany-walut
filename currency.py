87de82573c0c159363ed42bd23a1aebd5ae0bbac
Bartosz Szymczak <xbartosz2000@icloud.com>
2022-04-24 21:29:27 +0200

    Add comments

--- currency.py
+++ currency.py
@@ -2,30 +2,41 @@ from requests import get
 from bs4 import BeautifulSoup
 import random
 
+#Strona z danymi o kursach walutowych 
 URL = 'https://www.nbp.pl/home.aspx?f=/kursy/kursya.html'
+#Odpowiedz na zadanie GET ze strony
 page = get(URL)
+#Zawartosc strony
 bs = BeautifulSoup(page.content, "html.parser")
+#Tablica walut
 currencies = [] 
 
+#Klasa Currency definuje obiekt waluty
 class Currency:
 
+    #Atrybuty waluty takie jak nazwa, kod i wartosc
     name = " "
     code = " "
     value = 0.0
 
+    #Nadpisanie metody __str__
     def __str__(self):
         return self.name + " " + self.code + " " + str(self.value)
 
-
+#Klasa CurrencyData definuje obiekty danych walut
 class CurrencyData:
 
+    #Nadpianie konstruktora, tworzacego tablice walut
     def __init__(self):
         self.currencies = []
 
+    #Metoda zwracajca aktualna tablice walut
     def getCurrencies(self):
         return self.currencies
 
+    #Metoda sciagajaca aktualne dane walut
     def getData(self, currencies=None):
+        #Znalezienie odpowiednich elementow strony
         table = bs.find("table", class_='nbptable')
         body = table.find("tbody")
 
@@ -35,24 +46,23 @@ class CurrencyData:
             cur = Currency()
             for x in data.find_all("td"):
                 if(i==0):
+                    #Nazwa waluty
                     cur.name = x.get_text()
                 if(i==1):
+                    #Kod waluty
                     cur.code = x.get_text()
                 if(i==2):
+                    #Wartosc waluty
                     v = float(x.get_text().replace(',','.'))
                     r = random.uniform(0.95, 1.05)
                     cur.value = round(v*r,3)
                 i = i + 1
                 if(i==3):
                     i = 0
+                    #Dodanie waluty do tablicy
                     self.currencies.append(cur.__str__())
-                    # del cur
 
-    def saveData(self):
-        print(self.currencies[0])
-        for cur in self.currencies:
-            pass
-
+    #Metoda odpowiedzialna za wyswietlenie danych o walutach
     def showData(self):
         for cur in currencies:
             print(cur)
--- server.py
+++ server.py
@@ -5,47 +5,63 @@ import pickle
 
 cd = CurrencyData()
 
+#Dlugosc wiadomosci
 HEADER = 64
+#Port do wysylania wiadomosci
 PORT = 8000
+#IP serwera
 SERVER = socket.gethostbyname(socket.gethostname())
+#Struktura adresu
 ADDR = (SERVER, PORT)
+#Format kodowania
 FORMAT = 'utf-8'
 
+#Wiadomosc po ktorej klient zostaje rozlaczony
 DISCONNECT_MSG = "!DISCONNECT"
+#Wiadomosc po ktorej wysylana zostaje aktualizacja o danych walut
 REQUEST_DATA_MSG = "!REQ_DATA"
 
+#Stworzenie socketu serwera
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Przyedzielenie adresu do serwera
 server.bind(ADDR)
 
-
+#Funkcja obslugujaca laczacych sie klientow
 def handle_client(conn, addr):
     print(f"[NEW CONNETION] {addr} connected.")
     connected = True
     while connected:
+        #Odebranie dlugosci wiadomosci
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
+            #Dekodowanie wiadomosci
             msg = conn.recv(msg_length).decode(FORMAT)
+            #Odlaczenie klienta po otrzymaniu wlasciwej wiadomosci
             if msg == DISCONNECT_MSG:
                 connected = False
+            #Przeslanie wiadomosci o walutach po otrzymaniu wlasciwej wiadomosci
             if msg == REQUEST_DATA_MSG:
                 cd.getData()
                 data = pickle.dumps(cd.getCurrencies())
                 conn.send(data)
+        #Wypisanie logow
         print(f"[{addr}] - - {msg}")
-
+    #Zakonczenie polaczenia
     conn.close()
 
-
+#Funkcja rozpoczynajca dzialanie serwera
 def start():
     server.listen()
     print(f"[LISTENING] Server is listening in {SERVER}")
     while True:
+        #Akceptowanie nadchodzacego polaczenia klienta
         con, addr = server.accept()
+        #Kazdy klient jest oddzielnym watkiem
         thread = threading.Thread(target=handle_client, args=(con, addr))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
-
+#Info o dzialaniu serwera
 print("[STARTING] server is starting...")
 start()
