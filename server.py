1c1c77e27f4010f1b4638062b741e94d62474e61
Bartosz Szymczak <xbartosz2000@icloud.com>
2022-04-25 08:14:12 +0200

    Add basic client limit

--- server.py
+++ server.py
@@ -16,6 +16,8 @@ ADDR = (SERVER, PORT)
 #Format kodowania
 FORMAT = 'utf-8'
 
+#Ograniczona liczba klienow 
+MAX_CLIENTS = 3
 #Wiadomosc po ktorej klient zostaje rozlaczony
 DISCONNECT_MSG = "!DISCONNECT"
 #Wiadomosc po ktorej wysylana zostaje aktualizacja o danych walut
@@ -26,8 +28,19 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Przyedzielenie adresu do serwera
 server.bind(ADDR)
 
+def allow_new_client(num):
+    if(num < MAX_CLIENTS): return True
+    else: return False
+
 #Funkcja obslugujaca laczacych sie klientow
-def handle_client(conn, addr):
+def handle_client(conn, addr, is_ok):
+
+    if(is_ok == False):
+        #conn.send("CONN_REFUSED")
+        print(f"[CONNETION] {addr} refused.")
+        conn.close()
+        return
+
     print(f"[NEW CONNETION] {addr} connected.")
     connected = True
     while connected:
@@ -42,7 +55,8 @@ def handle_client(conn, addr):
                 connected = False
             #Przeslanie wiadomosci o walutach po otrzymaniu wlasciwej wiadomosci
             if msg == REQUEST_DATA_MSG:
-                cd.getData()
+                thread = threading.Thread(target=cd.getData())
+                thread.start()
                 data = pickle.dumps(cd.getCurrencies())
                 conn.send(data)
         #Wypisanie logow
@@ -55,12 +69,18 @@ def start():
     server.listen()
     print(f"[LISTENING] Server is listening in {SERVER}")
     while True:
-        #Akceptowanie nadchodzacego polaczenia klienta
-        con, addr = server.accept()
-        #Kazdy klient jest oddzielnym watkiem
-        thread = threading.Thread(target=handle_client, args=(con, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+        if(allow_new_client(threading.active_count() - 1) == True):
+            #Akceptowanie nadchodzacego polaczenia klienta
+            con, addr = server.accept()
+            #Kazdy klient jest oddzielnym watkiem
+            thread = threading.Thread(target=handle_client, args=(con, addr,True))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+        else:
+            con, addr = server.accept()
+            thread = threading.Thread(target=handle_client, args=(con, addr,False))
+            thread.start()
+
 
 #Info o dzialaniu serwera
 print("[STARTING] server is starting...")
